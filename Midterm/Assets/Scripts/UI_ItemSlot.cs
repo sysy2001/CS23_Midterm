3650d14e0efc88113ba69db5f4ce6aa1bdca1c75
hannahberman <77946983+hannahberman@users.noreply.github.com>
2023-02-27 20:05:22 -0500

    new car, street, prefab functionality

--- Midterm/Assets/Scripts/UI_ItemSlot.cs
+++ Midterm/Assets/Scripts/UI_ItemSlot.cs
@@ -6,6 +6,25 @@ using UnityEngine.EventSystems;
 
 public class UI_ItemSlot : MonoBehaviour, IDropHandler {
 
+       public GameObject item1;
+       public GameObject item2;
+       public GameObject item3;
+       public GameObject item4;
+       public GameObject item5;
+       public GameObject item6;
+
+
+       void Start(){
+              item1.SetActive(false);
+              item2.SetActive(false);
+              item3.SetActive(false);
+              item4.SetActive(false);
+              item5.SetActive(false);
+              item6.SetActive(false);
+       }
+
+
+
         public void OnDrop(PointerEventData eventData){
               if (eventData.pointerDrag != null){
                      Vector2 slotCenter = GetComponent<RectTransform>().anchoredPosition;
